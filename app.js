b2d19f4f2ed0245dca582b89ee2f0394cb4f5176
Eric Bush <eric.bush@mailbox.tu-dresden.de>
2020-08-15 09:36:44 +0200

    Added app.js

---
+++ app.js
@@ -0,0 +1,206 @@
+/* global window */
+import React, {Component} from 'react';
+import {render} from 'react-dom';
+import {StaticMap} from 'react-map-gl';
+import {AmbientLight, PointLight, LightingEffect} from '@deck.gl/core';
+import DeckGL from '@deck.gl/react';
+import {PolygonLayer} from '@deck.gl/layers';
+import {TripsLayer} from '@deck.gl/geo-layers';
+import {PathLayer} from '@deck.gl/layers';
+import {IconLayer} from '@deck.gl/layers';
+
+// Set your mapbox token here
+const MAPBOX_TOKEN = "pk.eyJ1IjoiZXJpY2J1c2giLCJhIjoiY2thcXVzMGszMmJhZjMxcDY2Y2FrdXkwMSJ9.cwBqtbXpWJbtAEGli1AIIg"; // eslint-disable-line
+
+// Source data CSV
+const DATA_URL = {
+  //BUILDINGS:
+  //  'https://raw.githubusercontent.com/visgl/deck.gl-data/master/examples/trips/buildings.json', // eslint-disable-line
+  //TRIPS: 'https://raw.githubusercontent.com/visgl/deck.gl-data/master/examples/trips/trips-v7.json' // eslint-disable-line
+  //TRIPS: 'https://raw.githubusercontent.com/ebaustria/coord_conversion/master/one_trace.json'
+  //TRIPS1: 'https://raw.githubusercontent.com/ebaustria/coord_conversion/master/one_trace.json'
+  ROUTES: 'https://raw.githubusercontent.com/ebaustria/regiaoSul/master/routes_brazil.json',
+  //ROUTES: 'https://raw.githubusercontent.com/ebaustria/coord_conversion/master/routes_bagepedroosorio.json',
+  //ROUTES: 'https://raw.githubusercontent.com/ebaustria/coord_conversion/master/route1_brazil.json',
+  TRIPS: 'https://raw.githubusercontent.com/ebaustria/regiaoSul/master/one_trace_brazil.json',
+  //STOPS: 'https://raw.githubusercontent.com/ebaustria/coord_conversion/master/single_stop_brazil.json'
+  STOPS: 'https://raw.githubusercontent.com/ebaustria/regiaoSul/master/stops_brazil.json'
+  //STOPS: 'https://raw.githubusercontent.com/ebaustria/coord_conversion/master/stops_brazil.json'
+};
+
+const ICON_MAPPING = {
+  marker: {x: 0, y: 0, width: 128, height: 128, mask: true}
+};
+
+const ambientLight = new AmbientLight({
+  color: [255, 255, 255],
+  intensity: 1.0
+});
+
+const pointLight = new PointLight({
+  color: [255, 255, 255],
+  intensity: 2.0,
+  position: [-74.05, 40.7, 8000]
+});
+
+const lightingEffect = new LightingEffect({ambientLight, pointLight});
+
+const material = {
+  ambient: 0.1,
+  diffuse: 0.6,
+  shininess: 32,
+  specularColor: [60, 64, 70]
+};
+
+const DEFAULT_THEME = {
+  buildingColor: [74, 80, 87],
+  trailColor0: [253, 128, 93],
+  trailColor1: [23, 184, 190],
+  material,
+  effects: [lightingEffect]
+};
+
+const INITIAL_VIEW_STATE = {
+  longitude: -52.789164,
+  latitude: -31.832282,
+  zoom: 13,
+  pitch: 45,
+  bearing: 0
+};
+
+const landCover = [[[-74.0, 40.7], [-74.02, 40.7], [-74.02, 40.72], [-74.0, 40.72]]];
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: 0
+    };
+  }
+
+  componentDidMount() {
+    this._animate();
+  }
+
+  componentWillUnmount() {
+    if (this._animationFrame) {
+      window.cancelAnimationFrame(this._animationFrame);
+    }
+  }
+
+  _animate() {
+    const {
+      loopLength = 604800, // unit corresponds to the timestamp in source data
+      animationSpeed = 30 // unit time per second
+    } = this.props;
+    const timestamp = Date.now() / 1000;
+    const loopTime = loopLength / animationSpeed;
+
+    this.setState({
+      time: ((timestamp % loopTime) / loopTime) * loopLength
+    });
+    this._animationFrame = window.requestAnimationFrame(this._animate.bind(this));
+  }
+
+  _renderLayers() {
+    const {
+      stops = DATA_URL.STOPS,
+      routes = DATA_URL.ROUTES,
+      buildings = DATA_URL.BUILDINGS,
+      trips = DATA_URL.TRIPS,
+      trailLength = 720,
+      theme = DEFAULT_THEME
+    } = this.props;
+
+    return [
+      // This is only needed when using shadow effects
+      new PolygonLayer({
+        id: 'ground',
+        data: landCover,
+        getPolygon: f => f,
+        stroked: false,
+        getFillColor: [0, 0, 0, 0]
+      }),
+      new PathLayer({
+        id: 'routes',
+        data: routes,
+        pickable: true,
+        widthScale: 20,
+        widthMinPixels: 3,
+        rounded: true,
+        getPath: e => e.path,
+        getColor: e => e.color, //colorToRGBArray(d.color),
+        opacity: 0.1,
+        getWidth: e => 1
+      }),
+      new TripsLayer({
+        id: 'trips',
+        data: trips,
+        getPath: d => d.path,
+        getTimestamps: d => d.timestamps,
+        getColor: d => (d.vendor === 0 ? theme.trailColor0 : theme.trailColor1),
+        opacity: 1,
+        widthMinPixels: 3,
+        rounded: true,
+        trailLength,
+        currentTime: this.state.time,
+
+        shadowEnabled: false
+      }),
+      new PolygonLayer({
+        id: 'buildings',
+        data: buildings,
+        extruded: true,
+        wireframe: false,
+        opacity: 0.5,
+        getPolygon: f => f.polygon,
+        getElevation: f => f.height,
+        getFillColor: theme.buildingColor,
+        material: theme.material
+      }),
+      new IconLayer({
+        id: 'stops',
+        data: stops,
+        pickable: true,
+        iconAtlas: 'https://raw.githubusercontent.com/visgl/deck.gl-data/master/website/icon-atlas.png',
+        iconMapping: ICON_MAPPING,
+        getIcon: g => 'marker',
+
+        sizeScale: 15,
+        getPosition: g => g.coordinates,
+        getSize: g => 3,
+        getColor: g => [255, 0, 0]
+      })
+    ];
+  }
+
+  render() {
+    const {
+      viewState,
+      mapStyle = 'mapbox://styles/mapbox/dark-v9',
+      theme = DEFAULT_THEME
+    } = this.props;
+
+    return (
+      <DeckGL
+        layers={this._renderLayers()}
+        effects={theme.effects}
+        initialViewState={INITIAL_VIEW_STATE}
+        viewState={viewState}
+        controller={true}
+        getTooltip={({object}) => object && object.name}
+      >
+        <StaticMap
+          reuseMaps
+          mapStyle={mapStyle}
+          preventStyleDiffing={true}
+          mapboxApiAccessToken={MAPBOX_TOKEN}
+        />
+      </DeckGL>
+    );
+  }
+}
+
+export function renderToDOM(container) {
+  render(<App />, container);
+}
