fe43174eca2ae30b70d0770a1d42a845a5b15eae
Eric Bush <eric.bush@mailbox.tu-dresden.de>
2020-09-14 20:51:48 +0200

    Added message creation function

--- json_generator.py
+++ json_generator.py
@@ -2,6 +2,7 @@ from lib import coord_conversion
 from lib import route_conversion
 from lib import stop_conversion
 from lib import arrival_conversion
+from lib import message_conversion
 
 print("Making trips JSON...")
 coord_conversion.make_trips("local_coordinates_brazil.txt", "gps_coordinates_brazil.csv")
@@ -14,3 +15,6 @@ stop_conversion.make_stops("gps_coordinates_brazil.csv")
 
 print("Making arrivals JSON...")
 arrival_conversion.make_arrivals("arrivals.txt", "gps_coordinates_brazil.csv")
+
+print("Making messages JSON...")
+message_conversion.message_json("created_messages.txt", "gps_coordinates_brazil.csv", "creating message")
--- lib/message_conversion.py
+++ lib/message_conversion.py
@@ -1,46 +1,25 @@
-import coord_conversion
+import lib.coord_conversion as cc
 import json
 
 
-def make_received(local_coordinates: str, gps_coordinates: str):
+def message_json(local_coordinates: str, gps_coordinates: str, notification: str):
     dict_list = []
 
-    received_timestamps = coord_conversion.timestamps_list(local_coordinates)
-    received_gps = coord_conversion.gps_list(gps_coordinates)
-    received_final_coords = coord_conversion.final_list(received_timestamps, received_gps)
+    message_timestamps = cc.timestamps_list(local_coordinates)
+    message_gps = cc.gps_list(gps_coordinates)
+    message_final_coords = cc.final_list(message_timestamps, message_gps)
 
-    for name, gps, timestamp, messages in received_final_coords:
+    for name, gps, timestamp in message_final_coords:
         new_dict = {}
         new_dict["name"] = name
         new_dict["coordinates"] = gps
         new_dict["timestamp"] = timestamp
-        new_dict["notification"] = "receiving message"
+        new_dict["notification"] = notification
 
         dict_list.append(new_dict)
 
+    file_name = notification + ".json"
     json_file = json.dumps(dict_list, indent=2)
 
-    with open("received_messages.json", "w") as file:
-        file.write(json_file)
-
-
-def carried_messages(local_coordinates: str, gps_coordinates: str):
-    dict_list = []
-
-    carrying_timestamps = coord_conversion.timestamps_list(local_coordinates)
-    carrying_gps = coord_conversion.gps_list(gps_coordinates)
-    carrying_final_coords = coord_conversion.final_list(carrying_timestamps, carrying_gps)
-
-    for name, gps, timestamp, messages in carrying_final_coords:
-        new_dict = {}
-        new_dict["name"] = name
-        new_dict["coordinates"] = gps
-        new_dict["timestamp"] = timestamp
-        new_dict["messages"] = messages
-
-        dict_list.append(new_dict)
-
-    json_file = json.dumps(dict_list, indent=2)
-
-    with open("carried_messages.json", "w") as file:
+    with open(file_name, "w") as file:
         file.write(json_file)
